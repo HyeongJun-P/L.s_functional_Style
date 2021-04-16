4ee0d14e31cc9229f5332b987a8d7c8761be2122
765prod <80656271+765prod@users.noreply.github.com>
2021-04-16 17:19:43 +0900

    use class state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import React from 'react';
-import FuncComp from './components/FuncComp';
-import ClassComp from './components/ClassComp';
+import './App.css'
+// import FuncComp from './components/FuncComp';
+// import ClassComp from './components/ClassComp';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       Hello Woirld
-      <FuncComp />
-      <ClassComp />
+      <FuncComp initNumber = {2}/>
+      <ClassComp  initNumber = {2}/>
     </div>
   );
+}
+
+function FuncComp(props) {
+  return (
+    <div className="container">
+      <h2> function style component</h2>
+      <p>Number : {props.initNumber}</p>
+    </div>
+  );
+}
+
+class ClassComp extends React.Component {
+  state = {
+    number : this.props.initNumber
+  }
+  render() {
+    return (
+      <div className="container">
+        <h2> class style component</h2>
+        <p>Number : {this.state.number}</p>
+        <input type="button" value="random" 
+        onClick={function() {
+          this.setState({
+            number: Math.floor(Math.random() * 99)
+          })
+        }.bind(this)}></input>
+      </div>
+    );
+  }
 }
 
 export default App;
