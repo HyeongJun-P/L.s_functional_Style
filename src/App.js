69dccb69bcc0d232f2ce43fcd7d3fc6f8de10188
Unknown-word <80656271+Unknown-word@users.noreply.github.com>
2021-04-16 09:25:41 +0900

    2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import './App.css';
+import FuncComp from './components/FuncComp';
+import ClassComp from './components/ClassComp';
 
 function App() {
   return (
     <div className="App">
-      
+      Hello Woirld
+      <FuncComp />
+      <ClassComp />
     </div>
   );
 }
