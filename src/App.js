d03cc95058794b227491727c5e1e1e675043e581
Your Name <you@example.com>
2021-04-22 09:24:40 +0900

    class_LifeCycle_final

--- src/App.js
+++ src/App.js
@@ -54,19 +54,22 @@ class ClassComp extends React.Component {
     number: this.props.initNumber,
     date: (new Date().toString())
   };
-  // componentWillMount() {
-  //   console.log('%cclass => componentWill<ount', classStyle)
-  // }
-  // componentDidMount() {
-  //   console.log('%cclass => componentDidMount',classStyle)
-  // }
-  // shouldComponentUpdate() {
-  //   console.log('%cclass => shouldComponentUpdate',classStyle)
-  // }
-  // componentWillUpdate() {
-  //   console.log('%cclass => componentWillUpdate',classStyle)
-
-  // }
+  componentWillMount() {
+    console.log('%cclass => componentWill<ount', classStyle);
+  }
+  componentDidMount() {
+    console.log('%cclass => componentDidMount',classStyle);
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('%cclass => shouldComponentUpdate',classStyle);
+    return true;
+  }
+  componentWillUpdate(nextProps, nextState) {
+    console.log('%cclass => componentWillUpdate',classStyle);
+  }
+  componentDidUpdate(nextProps, nextState) {
+    console.log("%cclass => componentDidUpdate",classStyle);
+  }
 
   // 커밋 수정됨
 
